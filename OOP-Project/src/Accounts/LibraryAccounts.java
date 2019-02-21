88820e04cf0664aa73992447c010a96b5ba00e2c
gomezr43@gator.uhd.edu <>
2019-02-21 17:18:51 -0600

    Merge origin/master
    Conflicts:
    	.gitignore

---
+++ OOP-Project/src/Accounts/LibraryAccounts.java
@@ -0,0 +1,15 @@
+/*
+ * This will manage library accounts
+ * such as add, edit, and delete accounts for the users
+ */
+package Accounts;
+
+/**
+ * Created Feb 21, 2019
+ * Modified: Feb 21, 2019
+ * @author Roberto Gomez
+ * @version 1
+ */
+public class LibraryAccounts {
+    
+}
