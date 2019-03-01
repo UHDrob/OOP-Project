2c66ceac22abfaf364d646e65c3ec414f9ee3e22
alansaavedra10 <37119790+alansaavedra10@users.noreply.github.com>
2019-03-01 10:45:18 -0600

    changed class name for ClassAccounts to classCustomers

--- OOP-Project/src/Customers/ClassCustomers.java
+++ OOP-Project/src/Customers/ClassCustomers.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Accounts;
+package Customers;
 
 /**
  *
- * @author Nina Lalonde
+ * @author Alan
  */
-public class Nina {
+public class ClassCustomers {
     
 }
